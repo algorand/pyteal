772d431f70434031986df1919230987fcf318ce9
Shumo Chu <shumo@algorand.com>
2020-02-10 13:55:43 -0500

    0.5.0

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name='pyteal', version='0.1', packages=find_packages())
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
+setuptools.setup(
+    name="pyteal", # Replace with your own username
+    version="0.5.0",
+    author="Shumo Chu",
+    author_email="shumo.chu@gmail.com",
+    description="Algorand Smart Contracts in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/algorand/pyteal",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
