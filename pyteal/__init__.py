9e3b8bf6ef880429cbdbad97a3ae662bf2053dd4
Jason Paulos <jasonpaulos@users.noreply.github.com>
2020-10-26 16:09:32 -0400

    Specify module exports

--- pyteal/__init__.py
+++ pyteal/__init__.py
@@ -1,7 +1,19 @@
 from .ast import *
+from .ast import __all__ as ast_all
 from .ir import *
+from .ir import __all__ as ir_all
 from .compiler import compileTeal
 from .types import TealType
 from .errors import TealInternalError, TealTypeError, TealInputError
 from .util import execute
 from .config import MAX_GROUP_SIZE
+
+__all__ = ast_all + ir_all + [
+    "compileTeal",
+    "TealType",
+    "TealInternalError",
+    "TealTypeError",
+    "TealInputError",
+    "execute",
+    "MAX_GROUP_SIZE",
+]
