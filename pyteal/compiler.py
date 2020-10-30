34197a8c783ff7d899bbcf4e3a1425df5e982937
Jason Paulos <jasonpaulos@users.noreply.github.com>
2020-10-30 15:19:31 -0400

    Add outgoing block check to compiler

--- pyteal/compiler.py
+++ pyteal/compiler.py
@@ -39,12 +39,17 @@ def flattenBlocks(blocks: List[TealBlock]) -> List[TealComponent]:
 
         if type(block) is TealSimpleBlock:
             simpleBlock = cast(TealSimpleBlock, block)
+            assert simpleBlock.nextBlock is not None
+
             nextIndex = blocks.index(simpleBlock.nextBlock, i+1)
             if nextIndex != i + 1:
                 references[nextIndex] += 1
                 code.append(TealOp(Op.b, indexToLabel(nextIndex)))
         elif type(block) is TealConditionalBlock:
             conditionalBlock = cast(TealConditionalBlock, block)
+            assert conditionalBlock.trueBlock is not None
+            assert conditionalBlock.falseBlock is not None
+
             trueIndex = blocks.index(conditionalBlock.trueBlock, i+1)
             falseIndex = blocks.index(conditionalBlock.falseBlock, i+1)
 
