eb2e689c48292571ee10bc334558a4b339e50667
Jason Paulos <jasonpaulos@users.noreply.github.com>
2020-11-12 11:31:16 -0500

    Update author details

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyteal", # Replace with your own username
+    name="pyteal",
     version="0.6.1",
-    author="Shumo Chu",
-    author_email="shumo.chu@gmail.com",
+    author="Algorand",
+    author_email="pypiservice@algorand.com",
     description="Algorand Smart Contracts in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
