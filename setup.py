7362a6bc1b0451966357729c5f5d0740e85b172f
Jason Paulos <jasonpaulos@users.noreply.github.com>
2021-08-31 13:29:37 -0700

    v0.8.0 (#105)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyteal",
-    version="0.8.0.dev",
+    version="0.8.0",
     author="Algorand",
     author_email="pypiservice@algorand.com",
     description="Algorand Smart Contracts in Python",
