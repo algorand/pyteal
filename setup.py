67f0a39af81c82de96810c18b09a16a797d678dd
Jason Paulos <jasonpaulos@users.noreply.github.com>
2021-11-08 15:20:06 -0800

    Include pyi files in build (#137)

--- setup.py
+++ setup.py
@@ -21,5 +21,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    package_data={"pyteal": ["*.pyi"]},
     python_requires=">=3.6",
 )
