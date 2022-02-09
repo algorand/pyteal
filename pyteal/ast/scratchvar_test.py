2e6ba7402c8c2d1dda4fa1be70ec4a51e03feea8
Zeph Grunschlag <zeph@algorand.com>
2022-02-08 20:16:54 -0600

    test cases

--- pyteal/ast/scratchvar_test.py
+++ pyteal/ast/scratchvar_test.py
@@ -11,12 +11,15 @@ options = CompileOptions()
 def test_scratchvar_init():
     myvar_default = ScratchVar()
     assert not myvar_default.slot.dynamic()
+    assert not myvar_default._subroutineInternal
 
     myvar_explicit = ScratchVar(slotId=42)
     assert not myvar_explicit.slot.dynamic()
+    assert not myvar_explicit._subroutineInternal
 
     myvar_from_stack = ScratchVar(slotId=Int(42))
     assert myvar_from_stack.slot.dynamic()
+    assert not myvar_from_stack._subroutineInternal
 
 
 def test_scratchvar_type():
