42200abe86159c6cefb2ae8ba6aa61e977b233e8
Zeph Grunschlag <zeph@algorand.com>
2022-02-18 12:59:20 -0600

    allow skipping new version in tests

--- tests/pass_by_ref_test.py
+++ tests/pass_by_ref_test.py
@@ -2,9 +2,10 @@ from pyteal import *
 
 from .compile_asserts import assert_new_v_old, compile_and_save
 
+# Set the following True, if don't want to run pass-by-ref dependent tests
+OLD_CODE_ONLY = False
+
 #### TESTS FOR PyTEAL THAT PREDATE PASS-BY-REF
-
-
 @Subroutine(TealType.bytes)
 def logcat(some_bytes, an_int):
     catted = ScratchVar(TealType.bytes)
@@ -75,110 +76,97 @@ def sub_even():
     )
 
 
-#### TESTS FOR NEW PyTEAL THAT USES PASS-BY-REF / DYNAMIC
+if not OLD_CODE_ONLY:
+    #### TESTS FOR NEW PyTEAL THAT USES PASS-BY-REF / DYNAMIC
+    @Subroutine(TealType.none)
+    def logcat_dynamic(first: ScratchVar, an_int):
+        return Seq(
+            first.store(Concat(first.load(), Itob(an_int))),
+            Log(first.load()),
+        )
 
+    def sub_logcat_dynamic():
+        first = ScratchVar(TealType.bytes)
+        return Seq(
+            first.store(Bytes("hello")),
+            logcat_dynamic(first, Int(42)),
+            Assert(Bytes("hello42") == first.load()),
+            Int(1),
+        )
 
-@Subroutine(TealType.none)
-def logcat_dynamic(first: ScratchVar, an_int):
-    return Seq(
-        first.store(Concat(first.load(), Itob(an_int))),
-        Log(first.load()),
-    )
+    def wilt_the_stilt():
+        player_score = DynamicScratchVar(TealType.uint64)
 
+        wilt = ScratchVar(TealType.uint64, 129)
+        kobe = ScratchVar(TealType.uint64)
+        dt = ScratchVar(TealType.uint64, 131)
 
-def sub_logcat_dynamic():
-    first = ScratchVar(TealType.bytes)
-    return Seq(
-        first.store(Bytes("hello")),
-        logcat_dynamic(first, Int(42)),
-        Assert(Bytes("hello42") == first.load()),
-        Int(1),
-    )
+        return Seq(
+            player_score.set_index(wilt),
+            player_score.store(Int(100)),
+            player_score.set_index(kobe),
+            player_score.store(Int(81)),
+            player_score.set_index(dt),
+            player_score.store(Int(73)),
+            Assert(player_score.load() == Int(73)),
+            Assert(player_score.index() == Int(131)),
+            player_score.set_index(wilt),
+            Assert(player_score.load() == Int(100)),
+            Assert(player_score.index() == Int(129)),
+            Int(100),
+        )
 
+    @Subroutine(TealType.none)
+    def swap(x: ScratchVar, y: ScratchVar):
+        z = ScratchVar(TealType.anytype)
+        return Seq(
+            z.store(x.load()),
+            x.store(y.load()),
+            y.store(z.load()),
+        )
 
-def wilt_the_stilt():
-    player_score = DynamicScratchVar(TealType.uint64)
+    @Subroutine(TealType.none)
+    def cat(x, y):
+        return Pop(Concat(x, y))
 
-    wilt = ScratchVar(TealType.uint64, 129)
-    kobe = ScratchVar(TealType.uint64)
-    dt = ScratchVar(TealType.uint64, 131)
+    def swapper():
+        a = ScratchVar(TealType.bytes)
+        b = ScratchVar(TealType.bytes)
+        return Seq(
+            a.store(Bytes("hello")),
+            b.store(Bytes("goodbye")),
+            cat(a.load(), b.load()),
+            swap(a, b),
+            Assert(a.load() == Bytes("goodbye")),
+            Assert(b.load() == Bytes("hello")),
+            Int(1000),
+        )
 
-    return Seq(
-        player_score.set_index(wilt),
-        player_score.store(Int(100)),
-        player_score.set_index(kobe),
-        player_score.store(Int(81)),
-        player_score.set_index(dt),
-        player_score.store(Int(73)),
-        Assert(player_score.load() == Int(73)),
-        Assert(player_score.index() == Int(131)),
-        player_score.set_index(wilt),
-        Assert(player_score.load() == Int(100)),
-        Assert(player_score.index() == Int(129)),
-        Int(100),
-    )
-
-
-@Subroutine(TealType.none)
-def swap(x: ScratchVar, y: ScratchVar):
-    z = ScratchVar(TealType.anytype)
-    return Seq(
-        z.store(x.load()),
-        x.store(y.load()),
-        y.store(z.load()),
-    )
-
-
-@Subroutine(TealType.none)
-def cat(x, y):
-    return Pop(Concat(x, y))
-
-
-def swapper():
-    a = ScratchVar(TealType.bytes)
-    b = ScratchVar(TealType.bytes)
-    return Seq(
-        a.store(Bytes("hello")),
-        b.store(Bytes("goodbye")),
-        cat(a.load(), b.load()),
-        swap(a, b),
-        Assert(a.load() == Bytes("goodbye")),
-        Assert(b.load() == Bytes("hello")),
-        Int(1000),
-    )
-
-
-@Subroutine(TealType.none)
-def factorial(n: ScratchVar):
-    tmp = ScratchVar(TealType.uint64)
-    return (
-        If(n.load() <= Int(1))
-        .Then(n.store(Int(1)))
-        .Else(
-            Seq(
-                tmp.store(n.load() - Int(1)),
-                factorial(tmp),
-                n.store(n.load() * tmp.load()),
+    @Subroutine(TealType.none)
+    def factorial(n: ScratchVar):
+        tmp = ScratchVar(TealType.uint64)
+        return (
+            If(n.load() <= Int(1))
+            .Then(n.store(Int(1)))
+            .Else(
+                Seq(
+                    tmp.store(n.load() - Int(1)),
+                    factorial(tmp),
+                    n.store(n.load() * tmp.load()),
+                )
             )
         )
-    )
 
-
-def fac_by_ref():
-    n = ScratchVar(TealType.uint64)
-    return Seq(
-        n.store(Int(42)),
-        factorial(n),
-        n.load(),
-    )
+    def fac_by_ref():
+        n = ScratchVar(TealType.uint64)
+        return Seq(
+            n.store(Int(42)),
+            factorial(n),
+            n.load(),
+        )
 
 
 OLD_CASES = (sub_logcat, sub_slowfib, sub_fastfib, sub_even)
-NEW_CASES = (sub_logcat_dynamic, swapper, wilt_the_stilt, fac_by_ref)
-
-
-def test_swapper():
-    compile_and_save(swapper)
 
 
 def test_old():
@@ -186,12 +174,20 @@ def test_old():
         assert_new_v_old(pt)
 
 
-def test_new():
-    for pt in NEW_CASES:
-        assert_new_v_old(pt)
-
-
 if __name__ == "__main__":
-    test_swapper()
     test_old()
-    test_new()
+
+
+if not OLD_CODE_ONLY:
+    NEW_CASES = (sub_logcat_dynamic, swapper, wilt_the_stilt, fac_by_ref)
+
+    def test_swapper():
+        compile_and_save(swapper)
+
+    def test_new():
+        for pt in NEW_CASES:
+            assert_new_v_old(pt)
+
+    if __name__ == "__main__":
+        test_swapper()
+        test_new()
