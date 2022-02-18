992d7dd1c1e041682f9a71497dc6889c69e5486b
Zeph Grunschlag <zeph@algorand.com>
2022-02-17 19:10:27 -0600

    comments

--- pyteal/ast/scratchvar.py
+++ pyteal/ast/scratchvar.py
@@ -93,7 +93,16 @@ class DynamicScratchVar:
         ttype: TealType = TealType.anytype,
         indexer: ScratchVar = None,
     ):
+        """Create a new DynamicScratchVar which references other ScratchVar's
 
+        Args:
+            ttype (optional): The type that this variable can hold. Defaults to TealType.anytype.
+            indexer (optional): A ScratchVar object that holds the _index_ that this DynamicScratchVar is
+                using to look up other slots. It is _NOT RECOMMENDED_ that a developer provide this optional
+                argument, but instead, to let the compiler construct one dynamically. In rare situations
+                it could be useful to provide the ability to explicitly provide the indexer. For example,
+                this is useful in handling pass-by-reference subroutine call semantics.
+        """
         self.type = ttype
         if indexer is None:
             indexer = ScratchVar(TealType.uint64)
@@ -121,20 +130,28 @@ class DynamicScratchVar:
         self.slot = self.indexer.slot
 
     def set_index(self, indexVar: ScratchVar) -> Expr:
+        """Set the this DynamicScratchVar to reference the provided `indexVar`.
+        Followup `store`, `load` and `index` operations will use the proved `indexVar` until
+        `set_index()` is called again on different ScratchVar.
+        """
         return self.indexer.store(indexVar.index())
 
     def store(self, value: Expr) -> Expr:
+        """Store the value in the referenced ScratchVar."""
         index = ScratchLoad(self.indexer.slot, TealType.uint64)
         return ScratchStore(slot=None, value=value, index_expression=index)
 
     def load(self) -> ScratchLoad:
+        """Load the current value from the referenced ScratchVar."""
         index = ScratchLoad(self.indexer.slot, TealType.uint64)
         return ScratchLoad(slot=None, type=self.type, index_expression=index)
 
     def index(self) -> Expr:
+        """Get the index of the referenced ScratchVar."""
         return self.indexer.load()
 
     def internal_index(self) -> Expr:
+        """Get the index of _this_ DynamicScratchVar, as opposed to that of the referenced ScratchVar."""
         return self.indexer.index()
 
 
