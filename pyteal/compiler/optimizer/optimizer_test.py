17543ed9f9118483c6234af20f694373cbbdc0b8
algoidurovic <91566643+algoidurovic@users.noreply.github.com>
2022-03-29 16:07:22 -0400

    add additional optimizer unit tests

--- pyteal/compiler/optimizer/optimizer_test.py
+++ pyteal/compiler/optimizer/optimizer_test.py
@@ -75,12 +75,12 @@ retsub
     assert actual == expected
 
 
-def test_optimize_subroutine_with_global_var():
-    global_var = ScratchVar(TealType.uint64)
+def test_optimize_subroutine_with_local_var():
+    local_var = ScratchVar(TealType.uint64)
 
     @Subroutine(TealType.uint64)
     def add(a1: Expr) -> Expr:
-        return Seq(global_var.store(Int(2)), global_var.load() + a1)
+        return Seq(local_var.store(Int(2)), local_var.load() + a1)
 
     program = Seq(
         [
@@ -147,12 +147,67 @@ retsub
     assert actual == expected
 
 
-def test_optimize_subroutine_with_reserved_global_var():
-    global_var = ScratchVar(TealType.uint64, 0)
+def test_optimize_subroutine_with_global_var():
+    global_var = ScratchVar(TealType.uint64)
 
     @Subroutine(TealType.uint64)
     def add(a1: Expr) -> Expr:
         return Seq(global_var.store(Int(2)), global_var.load() + a1)
+
+    program = Seq(
+        [
+            If(Txn.sender() == Global.creator_address()).Then(Pop(add(Int(1)))),
+            global_var.store(Int(5)),
+            Approve(),
+        ]
+    )
+
+    optimize_options = OptimizeOptions()
+
+    # unoptimized
+    expected = """#pragma version 4
+txn Sender
+global CreatorAddress
+==
+bz main_l2
+int 1
+callsub add_0
+pop
+main_l2:
+int 5
+store 0
+int 1
+return
+
+// add
+add_0:
+store 1
+int 2
+store 0
+load 0
+load 1
++
+retsub
+    """.strip()
+    actual = compileTeal(
+        program, version=4, mode=Mode.Application, optimize=optimize_options
+    )
+    assert actual == expected
+
+    # optimization should not apply to global vars
+    optimize_options = OptimizeOptions(scratch_slots=True)
+    actual = compileTeal(
+        program, version=4, mode=Mode.Application, optimize=optimize_options
+    )
+    assert actual == expected
+
+
+def test_optimize_subroutine_with_reserved_local_var():
+    local_var = ScratchVar(TealType.uint64, 0)
+
+    @Subroutine(TealType.uint64)
+    def add(a1: Expr) -> Expr:
+        return Seq(local_var.store(Int(2)), local_var.load() + a1)
 
     program = Seq(
         [
@@ -200,17 +255,16 @@ retsub
     assert actual == expected
 
 
-def test_optimize_subroutine_with_global_var_dependency():
+def test_optimize_subroutine_with_load_dependency():
     global_var = ScratchVar(TealType.uint64)
 
     @Subroutine(TealType.uint64)
-    def add(a1: Expr) -> Expr:
-        return Seq(global_var.store(Int(2)), global_var.load() + a1)
+    def add(a1: Expr, a2: Expr) -> Expr:
+        return Seq(Pop(a1 + a2), a2)
 
     program = Seq(
         [
-            If(Txn.sender() == Global.creator_address()).Then(Pop(add(Int(1)))),
-            Pop(global_var.load()),
+            If(Txn.sender() == Global.creator_address()).Then(Pop(add(Int(1), Int(2)))),
             Approve(),
         ]
     )
@@ -224,32 +278,52 @@ global CreatorAddress
 ==
 bz main_l2
 int 1
+int 2
 callsub add_0
 pop
 main_l2:
-load 0
-pop
 int 1
 return
 
 // add
 add_0:
 store 1
-int 2
 store 0
 load 0
 load 1
 +
-retsub
-    """.strip()
+pop
+load 1
+retsub""".strip()
     actual = compileTeal(
         program, version=4, mode=Mode.Application, optimize=optimize_options
     )
     assert actual == expected
 
-    # optimization shouldn't apply because of the dependency on the
-    # slot belonging to the global var.
-    optimize_options.scratch_slots = True
+    # slot 0 will get optimized but slot 1 won't because it has
+    # a load dependency.
+    expected = """#pragma version 4
+txn Sender
+global CreatorAddress
+==
+bz main_l2
+int 1
+int 2
+callsub add_0
+pop
+main_l2:
+int 1
+return
+
+// add
+add_0:
+store 0
+load 0
++
+pop
+load 0
+retsub""".strip()
+    optimize_options = OptimizeOptions(scratch_slots=True)
     actual = compileTeal(
         program, version=4, mode=Mode.Application, optimize=optimize_options
     )
@@ -297,6 +371,45 @@ app_global_get_ex
 pop
 int 1
 return""".strip()
+    optimize_options = OptimizeOptions(scratch_slots=True)
+    actual = compileTeal(
+        program, version=4, mode=Mode.Application, optimize=optimize_options
+    )
+    assert actual == expected
+
+
+def test_optimize_dynamic_var():
+    myvar = DynamicScratchVar()
+    regvar = ScratchVar()
+    program = Seq(
+        regvar.store(Int(1)),
+        myvar.set_index(regvar),
+        regvar.store(Int(2)),
+        Pop(regvar.load()),
+        Approve(),
+    )
+
+    optimize_options = OptimizeOptions()
+
+    # unoptimized
+    expected = """#pragma version 4
+int 1
+store 1
+int 1
+store 0
+int 2
+store 1
+load 1
+pop
+int 1
+return""".strip()
+    actual = compileTeal(
+        program, version=4, mode=Mode.Application, optimize=optimize_options
+    )
+    assert actual == expected
+
+    # optimization should not change the code because the candidate slot
+    # is used by the dynamic slot variable.
     optimize_options = OptimizeOptions(scratch_slots=True)
     actual = compileTeal(
         program, version=4, mode=Mode.Application, optimize=optimize_options
