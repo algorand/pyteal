a05a28951d090a28742cc14631ed6f21111947a4
Jason Paulos <jasonpaulos@users.noreply.github.com>
2022-04-13 15:27:14 -0700

    Add readthedocs config file

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "PyTeal"
-copyright = "2021, Algorand"
+copyright = "2022, Algorand"
 author = "Algorand"
 
 
