bca53aaddd75e3e6aa5b2027de72c4d938b79e6a
michaeldiamant <michaeldiamant@users.noreply.github.com>
2022-04-15 15:27:42 -0400

    Bump missed version reference

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyteal",
-    version="0.11.0",
+    version="0.11.1",
     author="Algorand",
     author_email="pypiservice@algorand.com",
     description="Algorand Smart Contracts in Python",
