189a58411c1eb35785c24cce38aba2a1cbe3963d
algoidurovic <91566643+algoidurovic@users.noreply.github.com>
2022-04-20 12:32:56 -0400

    changes to compy with flake8

--- pyteal/ast/opup_test.py
+++ pyteal/ast/opup_test.py
@@ -2,33 +2,33 @@ import pytest
 
 from pyteal.ast.opup import OpUp, OpUpMode
 
-from .. import *
+import pyteal as pt
 
 
 def test_opup_explicit():
     mode = OpUpMode.Explicit
-    with pytest.raises(TealInputError) as err:
+    with pytest.raises(pt.TealInputError) as err:
         opup = OpUp(mode)
     assert "target_app_id must be specified in Explicit OpUp mode" in str(err.value)
 
-    with pytest.raises(TealTypeError):
-        opup = OpUp(mode, Bytes("appid"))
+    with pytest.raises(pt.TealTypeError):
+        opup = OpUp(mode, pt.Bytes("appid"))
 
-    opup = OpUp(mode, Int(1))
+    opup = OpUp(mode, pt.Int(1))
 
-    with pytest.raises(TealTypeError):
-        opup.ensure_budget(Bytes("budget"))
+    with pytest.raises(pt.TealTypeError):
+        opup.ensure_budget(pt.Bytes("budget"))
 
-    with pytest.raises(TealTypeError):
-        opup.maximize_budget(Bytes("fee"))
+    with pytest.raises(pt.TealTypeError):
+        opup.maximize_budget(pt.Bytes("fee"))
 
-    assert opup.target_app_id == Int(1)
+    assert opup.target_app_id == pt.Int(1)
 
     # verify correct usage doesn't cause an error
-    program_with_ensure = Seq(opup.ensure_budget(Int(500) + Int(1000)), Return(Int(1)))
+    _ = pt.Seq(opup.ensure_budget(pt.Int(500) + pt.Int(1000)), pt.Return(pt.Int(1)))
 
-    program_with_maximize = Seq(
-        opup.maximize_budget(Txn.fee() - Int(100)), Return(Int(1))
+    _ = pt.Seq(
+        opup.maximize_budget(pt.Txn.fee() - pt.Int(100)), pt.Return(pt.Int(1))
     )
 
 
@@ -36,18 +36,18 @@ def test_opup_oncall():
     mode = OpUpMode.OnCall
     opup = OpUp(mode)
 
-    with pytest.raises(TealTypeError):
-        opup.ensure_budget(Bytes("budget"))
+    with pytest.raises(pt.TealTypeError):
+        opup.ensure_budget(pt.Bytes("budget"))
 
-    with pytest.raises(TealTypeError):
-        opup.maximize_budget(Bytes("fee"))
+    with pytest.raises(pt.TealTypeError):
+        opup.maximize_budget(pt.Bytes("fee"))
 
     # verify correct usage doesn't cause an error
-    program_with_ensure = Seq(opup.ensure_budget(Int(500) + Int(1000)), Return(Int(1)))
+    _ = pt.Seq(opup.ensure_budget(pt.Int(500) + pt.Int(1000)), pt.Return(pt.Int(1)))
 
     assert opup.target_app_id is not None
     assert opup.target_app_id_slot is not None
 
-    program_with_maximize = Seq(
-        opup.maximize_budget(Txn.fee() - Int(100)), Return(Int(1))
+    _ = pt.Seq(
+        opup.maximize_budget(pt.Txn.fee() - pt.Int(100)), pt.Return(pt.Int(1))
     )
