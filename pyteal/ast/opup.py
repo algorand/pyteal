399a1b915f77dae95eaee41f5837e31c6d5d9ee3
algoidurovic <91566643+algoidurovic@users.noreply.github.com>
2022-04-21 14:56:34 -0400

    Update pyteal/ast/opup.py
    Co-authored-by: Jason Paulos <jasonpaulos@users.noreply.github.com>

--- pyteal/ast/opup.py
+++ pyteal/ast/opup.py
@@ -29,7 +29,7 @@ class OpUpMode(Enum):
     OnCall = 1
 
 
-ON_CALL_APP = Bytes("base16", "06810143")  # v6 pyteal program "Int(1)"
+ON_CALL_APP = Bytes("base16", "068101")  # v6 teal program "int 1"
 MIN_TXN_FEE = Int(1000)
 
 
