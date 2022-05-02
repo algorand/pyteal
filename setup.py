9b4e72fa74f147adf0ac8a747a00815ea081747a
Zeph Grunschlag <tzaffi@users.noreply.github.com>
2022-05-02 16:27:50 -0500

    De emojify (#316)
    * no more alpacas

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyteal",
-    version="0.12.0",
+    version="0.12.1b1",
     author="Algorand",
     author_email="pypiservice@algorand.com",
     description="Algorand Smart Contracts in Python",
@@ -21,7 +21,7 @@ setuptools.setup(
             "black==22.3.0",
             "flake8==4.0.1",
             "flake8-tidy-imports==4.6.0",
-            "graviton@git+https://github.com/algorand/graviton@🦙",
+            "graviton@git+https://github.com/algorand/graviton@5549e6227a819b9f6d346f407aed32f4976ec0b2",
             "mypy==0.950",
             "pytest==7.1.1",
             "pytest-cov==3.0.0",
