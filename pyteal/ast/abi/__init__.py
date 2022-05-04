ef61b2c5c9067a8cbb0fc5a756759264c1046605
Jason Paulos <jasonpaulos@users.noreply.github.com>
2022-05-04 14:51:55 -0700

    Add convenience `make` method for ABI types (#326)
    * Add make method for abi types
    
    * Respondn to feedback

--- pyteal/ast/abi/__init__.py
+++ pyteal/ast/abi/__init__.py
@@ -34,8 +34,9 @@ from pyteal.ast.abi.tuple import (
 from pyteal.ast.abi.array_base import ArrayTypeSpec, Array, ArrayElement
 from pyteal.ast.abi.array_static import StaticArrayTypeSpec, StaticArray
 from pyteal.ast.abi.array_dynamic import DynamicArrayTypeSpec, DynamicArray
-from pyteal.ast.abi.util import type_spec_from_annotation
+
 from pyteal.ast.abi.method_return import MethodReturn
+from pyteal.ast.abi.util import type_spec_from_annotation, make
 
 __all__ = [
     "String",
@@ -76,6 +77,7 @@ __all__ = [
     "StaticArray",
     "DynamicArrayTypeSpec",
     "DynamicArray",
-    "type_spec_from_annotation",
     "MethodReturn",
+    "type_spec_from_annotation",
+    "make",
 ]
