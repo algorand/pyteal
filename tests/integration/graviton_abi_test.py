da163d6ef441983af8cc750596ac045ffee7db7a
Zeph Grunschlag <zeph@algorand.com>
2022-05-06 18:15:22 -0500

    complex conjugate

--- tests/integration/graviton_abi_test.py
+++ tests/integration/graviton_abi_test.py
@@ -318,6 +318,19 @@ def complex130_imag(x: Complex130, *, output: Int65):
     return output.set(x[1])
 
 
+@Blackbox(input_types=[None])
+@ABIReturnSubroutine
+def complex130_conjugate(x: Complex130, *, output: Complex130):
+    z0 = pt.abi.make(Int65)
+    z1 = pt.abi.make(Int65)
+    return pt.Seq(
+        z0.set(complex130_real(x)),
+        z1.set(complex130_imag(x)),
+        z1.set(int65_negate(z1)),
+        output.set(z0, z1),
+    )
+
+
 def test_integer65():
     bbpt_subtract_slick = BlackboxPyTealer(int65_sub, pt.Mode.Application)
     approval_subtract_slick = bbpt_subtract_slick.program()
@@ -461,6 +474,12 @@ def test_complex130():
         approval_cplx_imag(), pt.Mode.Application, version=6
     )
 
+    bbpt_complex_conjugate = BlackboxPyTealer(complex130_conjugate, pt.Mode.Application)
+    approval_cplx_conjugate = bbpt_complex_conjugate.program()
+    teal_cplx_conjugate = pt.compileTeal(
+        approval_cplx_conjugate(), pt.Mode.Application, version=6
+    )
+
     unary_abi_argument_types = bbpt_complex_real.abi_argument_types()
     binary_abi_argument_types = bbpt_cplx_add.abi_argument_types()
 
@@ -537,6 +556,14 @@ def test_complex130():
         real_abi_return_type,
     )
 
+    inspectors_cplx_conjugate = DryRunExecutor.dryrun_app_on_sequence(
+        algod,
+        teal_cplx_conjugate,
+        unary_inputs,
+        unary_abi_argument_types,
+        complex_abi_return_type,
+    )
+
     for i in range(N):
         binary_args = binary_inputs[i]
         x, y = tuple(map(pytuple_to_complex, binary_args))
@@ -556,6 +583,8 @@ def test_complex130():
 
         inspector_cplx_imag = inspectors_cplx_imag[i]
 
+        inspector_cplx_conjugate = inspectors_cplx_conjugate[i]
+
         assert x + y == pytuple_to_complex(
             inspector_cplx_add.last_log()
         ), inspector_cplx_add.report(binary_args, f"failed for {binary_args}", row=i)
@@ -571,3 +600,9 @@ def test_complex130():
         assert u.imag == pytuple_to_int(
             inspector_cplx_imag.last_log()
         ), inspector_cplx_imag.report(unary_args, f"failed for {unary_args}", row=i)
+
+        assert u.conjugate() == pytuple_to_complex(
+            inspector_cplx_conjugate.last_log()
+        ), inspector_cplx_conjugate.report(
+            unary_args, f"failed for {unary_args}", row=i
+        )
