abe02cc70e29dd974fbcc20b9fce6ac24b8cf6e6
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-05-16 09:10:18 -0400

    rename and add a couple tests

--- pyteal/ast/abi/__init__.py
+++ pyteal/ast/abi/__init__.py
@@ -34,7 +34,7 @@ from pyteal.ast.abi.tuple import (
 from pyteal.ast.abi.array_base import ArrayTypeSpec, Array, ArrayElement
 from pyteal.ast.abi.array_static import StaticArrayTypeSpec, StaticArray
 from pyteal.ast.abi.array_dynamic import DynamicArrayTypeSpec, DynamicArray
-from pyteal.ast.abi.reference_types import (
+from pyteal.ast.abi.reference_type import (
     Account,
     AccountTypeSpec,
     Asset,
