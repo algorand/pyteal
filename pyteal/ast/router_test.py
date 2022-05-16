407f3d1d6db96e2778e805a7a9162366f2814c28
Hang Su <hang.su@algorand.com>
2022-05-16 16:50:31 -0400

    start testcases

--- pyteal/ast/router_test.py
+++ pyteal/ast/router_test.py
@@ -1,1 +1,111 @@
-# TODO
+# import pytest
+import pyteal as pt
+
+options = pt.CompileOptions(version=5)
+
+
+@pt.ABIReturnSubroutine
+def add(a: pt.abi.Uint64, b: pt.abi.Uint64, *, output: pt.abi.Uint64) -> pt.Expr:
+    return output.set(a.get() + b.get())
+
+
+@pt.ABIReturnSubroutine
+def sub(a: pt.abi.Uint64, b: pt.abi.Uint64, *, output: pt.abi.Uint64) -> pt.Expr:
+    return output.set(a.get() - b.get())
+
+
+@pt.ABIReturnSubroutine
+def mul(a: pt.abi.Uint64, b: pt.abi.Uint64, *, output: pt.abi.Uint64) -> pt.Expr:
+    return output.set(a.get() * b.get())
+
+
+@pt.ABIReturnSubroutine
+def div(a: pt.abi.Uint64, b: pt.abi.Uint64, *, output: pt.abi.Uint64) -> pt.Expr:
+    return output.set(a.get() / b.get())
+
+
+@pt.ABIReturnSubroutine
+def mod(a: pt.abi.Uint64, b: pt.abi.Uint64, *, output: pt.abi.Uint64) -> pt.Expr:
+    return output.set(a.get() % b.get())
+
+
+@pt.ABIReturnSubroutine
+def qrem(
+    a: pt.abi.Uint64,
+    b: pt.abi.Uint64,
+    *,
+    output: pt.abi.Tuple2[pt.abi.Uint64, pt.abi.Uint64],
+) -> pt.Expr:
+    return pt.Seq(
+        (q := pt.abi.Uint64()).set(a.get() / b.get()),
+        (rem := pt.abi.Uint64()).set(a.get() % b.get()),
+        output.set(q, rem),
+    )
+
+
+@pt.ABIReturnSubroutine
+def reverse(a: pt.abi.String, *, output: pt.abi.String) -> pt.Expr:
+    idx = pt.ScratchVar()
+    buff = pt.ScratchVar()
+
+    init = idx.store(pt.Int(0))
+    cond = idx.load() < a.length()
+    iter = idx.store(idx.load() + pt.Int(1))
+    return pt.Seq(
+        buff.store(pt.Bytes("")),
+        pt.For(init, cond, iter).Do(
+            a[idx.load()].use(lambda v: buff.store(pt.Concat(v.encode(), buff.load())))
+        ),
+        output.set(buff.load()),
+    )
+
+
+@pt.ABIReturnSubroutine
+def concat_strings(
+    b: pt.abi.DynamicArray[pt.abi.String], *, output: pt.abi.String
+) -> pt.Expr:
+    idx = pt.ScratchVar()
+    buff = pt.ScratchVar()
+
+    init = idx.store(pt.Int(0))
+    cond = idx.load() < b.length()
+    iter = idx.store(idx.load() + pt.Int(1))
+    return pt.Seq(
+        buff.store(pt.Bytes("")),
+        pt.For(init, cond, iter).Do(
+            b[idx.load()].use(lambda s: buff.store(pt.Concat(buff.load(), s.get())))
+        ),
+        output.set(buff.load()),
+    )
+
+
+@pt.ABIReturnSubroutine
+def manyargs(
+    a: pt.abi.Uint64,
+    b: pt.abi.Uint64,
+    c: pt.abi.Uint64,
+    d: pt.abi.Uint64,
+    e: pt.abi.Uint64,
+    f: pt.abi.Uint64,
+    g: pt.abi.Uint64,
+    h: pt.abi.Uint64,
+    i: pt.abi.Uint64,
+    j: pt.abi.Uint64,
+    k: pt.abi.Uint64,
+    l: pt.abi.Uint64,
+    m: pt.abi.Uint64,
+    n: pt.abi.Uint64,
+    o: pt.abi.Uint64,
+    p: pt.abi.Uint64,
+    q: pt.abi.Uint64,
+    r: pt.abi.Uint64,
+    s: pt.abi.Uint64,
+    t: pt.abi.Uint64,
+    *,
+    output: pt.abi.Uint64,
+) -> pt.Expr:
+    return output.set(a.get())
+
+
+def test():
+    pass
