4b97ff53873df600822cc8a339f4bc8bc506282d
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-05-23 14:13:38 -0400

    fmt

--- pyteal/ast/box.py
+++ pyteal/ast/box.py
@@ -2,22 +2,41 @@ from typing import TYPE_CHECKING
 from pyteal.errors import TealInputError
 
 from pyteal.types import TealType, require_type
-from pyteal.ir import TealOp, Op, TealBlock, TealSimpleBlock, TealConditionalBlock
+from pyteal.ir import TealOp, Op, TealBlock
 from pyteal.ast.expr import Expr
 
 if TYPE_CHECKING:
     from pyteal.compiler import CompileOptions
 
+
+class Box:
+    @staticmethod
+    def create(name: Expr, size: Expr) -> "BoxCreate":
+        return BoxCreate(name, size)
+
+    @staticmethod
+    def delete(name: Expr) -> "BoxDelete":
+        return BoxDelete(name)
+
+    @staticmethod
+    def extract(name: Expr, start: Expr, stop: Expr) -> "BoxExtract":
+        return BoxExtract(name, start, stop)
+
+    @staticmethod
+    def replace(name: Expr, start: Expr, value: Expr) -> "BoxReplace":
+        return BoxReplace(name, start, value)
+
+
 class BoxCreate(Expr):
-    """"""
+    """Create a box with a given name and size."""
 
     def __init__(self, name: Expr, size: Expr) -> None:
-        """Create an assert statement that raises an error if the condition is false.
-
-        Args:
-            size: The number of bytes to reserve for this box. Must evaluate to a uint64.
-            name: The key used to reference this box. Must evaluate to a bytes.
         """
+        Args:
+            name: The key used to reference this box. Must evaluate to a bytes.
+            size: The number of bytes to reserve for this box. Must evaluate to a uint64.
+        """
+
         super().__init__()
         require_type(name, TealType.bytes)
         require_type(size, TealType.uint64)
@@ -35,7 +54,7 @@ class BoxCreate(Expr):
         )
 
     def __str__(self):
-        return "(box_create {})".format(self.size, self.name)
+        return "(box_create {} {})".format(self.name, self.size)
 
     def type_of(self):
         return TealType.none
@@ -48,10 +67,13 @@ BoxCreate.__module__ = "pyteal"
 
 
 class BoxDelete(Expr):
-    """"""
+    """Deletes a box given it's name"""
 
     def __init__(self, name: Expr) -> None:
-        """ """
+        """
+        Args:
+            name: The key the box was created with. Must evaluate to bytes
+        """
         super().__init__()
         require_type(name, TealType.bytes)
         self.name = name
@@ -62,9 +84,7 @@ class BoxDelete(Expr):
                 f"BoxDelete not available on teal version {options.version} (first available {Op.box_del.min_version})"
             )
 
-        return TealBlock.FromOp(
-            options, TealOp(self, Op.box_del),  self.name
-        )
+        return TealBlock.FromOp(options, TealOp(self, Op.box_del), self.name)
 
     def __str__(self):
         return "(box_del {})".format(self.name)
@@ -80,10 +100,15 @@ BoxDelete.__module__ = "pyteal"
 
 
 class BoxReplace(Expr):
-    """"""
+    """Replaces bytes in a box given its name, start index, and value"""
 
     def __init__(self, name: Expr, start: Expr, value: Expr) -> None:
-        """ """
+        """
+        Args:
+            name: The key the box was created with. Must evaluate to bytes
+            start: The byte index into the box to start writing. Must evaluate to uint64
+            value: The value to start writing at start index. Must evaluate to bytes
+        """
         super().__init__()
         require_type(name, TealType.bytes)
         require_type(start, TealType.uint64)
@@ -116,10 +141,16 @@ BoxReplace.__module__ = "pyteal"
 
 
 class BoxExtract(Expr):
-    """"""
+    """Extracts bytes in a box given its name, start index and stop index."""
 
     def __init__(self, name: Expr, start: Expr, stop: Expr) -> None:
-        """ """
+        """
+        Args:
+            name: The key the box was created with. Must evaluate to bytes
+            start: The byte index into the box to start reading. Must evaluate to uint64
+            stop: The byte index into the box to stop reading. Must evaluate to uint64
+        """
+
         super().__init__()
         require_type(name, TealType.bytes)
         require_type(start, TealType.uint64)
