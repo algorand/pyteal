e4bad03de780f6a298d6e18d295a072dc41b0724
michaeldiamant <michaeldiamant@users.noreply.github.com>
2022-05-23 13:08:38 -0400

    Update prototype to reflect on create vs on call

--- pyteal/ast/router_switchboard.py
+++ pyteal/ast/router_switchboard.py
@@ -5,6 +5,7 @@ from pyteal.ast.subroutine import (
 )
 from pyteal.ast.expr import Expr
 from pyteal.ast.int import Int
+from typing import Optional
 import pyteal as pt
 
 # router_switchboard.py outlines an approach attempting to meet these constraints:
@@ -15,23 +16,45 @@ import pyteal as pt
 # Create special-purpose decorators for ARC-4 entry point definition + registration.  Though it's possible to imagine extending the approach and/or providing a parallel _router_ construct.
 
 
-# BareAppCalls deliberately provides the least-feature rich possible API.  It makes no attempt to define specific behaviors (e.g. creator can update).
+@dataclass(frozen=True)
+class BareAppCall:
+    on_create: Optional[SubroutineFnWrapper]
+    on_call: Optional[SubroutineFnWrapper]
+
+    @staticmethod
+    def none() -> "BareAppCall":
+        return BareAppCall(None, None)
+
+    @staticmethod
+    def only_on_create(f: SubroutineFnWrapper) -> "BareAppCall":
+        return BareAppCall(f, None)
+
+    @staticmethod
+    def only_on_call(f: SubroutineFnWrapper) -> "BareAppCall":
+        return BareAppCall(None, f)
+
+    @staticmethod
+    def always(f: SubroutineFnWrapper) -> "BareAppCall":
+        return BareAppCall(f, f)
+
+
+# BareAppCallSpecification deliberately provides the least-feature rich possible API.  It makes no attempt to define specific behaviors (e.g. creator can update).
 #
 # Instead, it's up to the caller to specify the desired behavior.  Additionally, it's possible to imagine defiining specific `Subroutine`s as a way to predefine behaviors.
 @dataclass(frozen=True)
-class BareAppCalls:
-    close_out: SubroutineFnWrapper
-    clear_state: SubroutineFnWrapper
-    delete_application: SubroutineFnWrapper
-    no_op: SubroutineFnWrapper
-    opt_in: SubroutineFnWrapper
-    update_application: SubroutineFnWrapper
+class BareAppCallSpecification:
+    close_out: BareAppCall
+    clear_state: BareAppCall
+    delete_application: BareAppCall
+    no_op: BareAppCall
+    opt_in: BareAppCall
+    update_application: BareAppCall
 
 
 # Switchboard exposes a way to build the AST provided ARC-4 Contract inputs.
 class Switchboard:
     @staticmethod
-    def _validate_bare_app_calls(bac: BareAppCalls) -> None:
+    def _validate_bare_app_calls(bacs: BareAppCallSpecification) -> None:
         # Imagine validation raising an error when:
         # * a parameter list length > 0
         # * a return value != none
@@ -47,9 +70,9 @@ class Switchboard:
     # pattern.  Doing so, limits validation cases.
     @staticmethod
     def new_ast(
-        app_name: str, bare_app_calls: BareAppCalls, methods: set[ABIReturnSubroutine]
+        app_name: str, bacs: BareAppCallSpecification, methods: set[ABIReturnSubroutine]
     ) -> Expr:
-        Switchboard._validate_bare_app_calls(bare_app_calls)
+        Switchboard._validate_bare_app_calls(bacs)
         Switchboard._validate_methods(methods)
 
         # Build AST
@@ -84,13 +107,13 @@ print(
     pt.compileTeal(
         ast=Switchboard.new_ast(
             app_name="my_1st_app",
-            bare_app_calls=BareAppCalls(
-                close_out=bare_approval,
-                clear_state=bare_approval,
-                delete_application=bare_approval_by_creator,
-                no_op=bare_reject,
-                opt_in=bare_approval,
-                update_application=bare_approval_by_creator,
+            bacs=BareAppCallSpecification(
+                close_out=BareAppCall.none(),
+                clear_state=BareAppCall.only_on_call(bare_approval_by_creator),
+                delete_application=BareAppCall.only_on_create(bare_approval_by_creator),
+                no_op=BareAppCall.always(bare_reject),
+                opt_in=BareAppCall.always(bare_approval),
+                update_application=BareAppCall.only_on_call(bare_approval_by_creator),
             ),
             methods={
                 sum,
