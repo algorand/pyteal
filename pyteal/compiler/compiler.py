c8cece4d39e770c8ef77bfd578f8e857b138375b
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-05-27 15:09:18 -0400

    using the trace from previous ast

--- pyteal/compiler/compiler.py
+++ pyteal/compiler/compiler.py
@@ -128,9 +128,20 @@ def compileSubroutine(
 
     if not ast.has_return():
         if ast.type_of() == TealType.none:
-            ast = Seq([ast, Return()])
+            ret_expr = Return()
+            ret_expr.trace = ast.trace
+
+            seq_expr = Seq([ast, ret_expr])
+            seq_expr.trace = ast.trace
+
+            ast = seq_expr 
         else:
-            ast = Return(ast)
+            ret_expr = Return()
+            ret_expr.trace = ast.trace
+            ret_expr.value = ast
+            ast = ret_expr
+
+
 
     options.setSubroutine(currentSubroutine)
     start, end = ast.__teal__(options)
