531fe7db32f36acc47421a0a7432c13d95a91807
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-06-06 11:47:36 -0400

    fmt, lint

--- pyteal/ast/abi/reference_type.py
+++ pyteal/ast/abi/reference_type.py
@@ -1,18 +1,111 @@
-from pyteal.ast.abi.uint import Uint, UintTypeSpec
+from typing import Final, TypeVar, Union, cast
+from abc import abstractmethod
+from pyteal.ast.abi.type import BaseType, ComputedValue, TypeSpec
+
+from pyteal.ast.binaryexpr import GetByte
+from pyteal.ast.bytes import Bytes
+from pyteal.ast.int import Int
 from pyteal.ast.expr import Expr
+from pyteal.ast.ternaryexpr import SetByte
 from pyteal.ast.txn import Txn
+from pyteal.errors import TealInputError
+from pyteal.types import TealType
 
 
-class ReferenceTypeSpec():
-    pass
+T = TypeVar("T", bound="ReferenceType")
 
-class ReferenceType():
-    pass
 
-class AccountTypeSpec(UintTypeSpec):
-    def __init__(self):
-        super().__init__(8)
+class ReferenceTypeSpec(TypeSpec):
+    def __init__(self) -> None:
+        super().__init__()
+        self.size: Final = 8
 
+    @abstractmethod
+    def new_instance(self) -> "ReferenceType":
+        pass
+
+    @abstractmethod
+    def annotation_type(self) -> "type[ReferenceType]":
+        pass
+
+    def bit_size(self) -> int:
+        """Get the bit size of this uint type"""
+        return self.size
+
+    def is_dynamic(self) -> bool:
+        return False
+
+    def byte_length_static(self) -> int:
+        return 1
+
+    def storage_type(self) -> TealType:
+        return TealType.uint64
+
+    def __eq__(self, other: object) -> bool:
+        return type(self) is type(other)
+
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+
+
+class ReferenceType(BaseType):
+    @abstractmethod
+    def __init__(self, spec: ReferenceTypeSpec) -> None:
+        super().__init__(spec)
+
+    def type_spec(self) -> ReferenceTypeSpec:
+        return cast(ReferenceTypeSpec, super().type_spec())
+
+    def get(self) -> Expr:
+        return self.stored_value.load()
+
+    def set(
+        self: T, value: Union[int, Expr, "ReferenceType", ComputedValue[T]]
+    ) -> Expr:
+        if isinstance(value, ComputedValue):
+            return self._set_with_computed_type(value)
+
+        if isinstance(value, BaseType) and not (
+            isinstance(value.type_spec(), ReferenceTypeSpec)
+        ):
+            raise TealInputError(
+                "Type {} is not assignable to type {}".format(
+                    value.type_spec(), self.type_spec()
+                )
+            )
+
+        match value:
+            case int():
+                return self.stored_value.store(Int(value))
+            case Expr():
+                return self.stored_value.store(value)
+            case ReferenceType():
+                return self.stored_value.store(value.get())
+            case _:
+                raise TealInputError(
+                    "Expected int, Expr, ReferenceType or ComputedValue, got {}".format(
+                        type(value)
+                    )
+                )
+
+    def decode(
+        self,
+        encoded: Expr,
+        *,
+        startIndex: Expr = None,
+        endIndex: Expr = None,
+        length: Expr = None,
+    ) -> Expr:
+        if startIndex is None:
+            startIndex = Int(0)
+        return self.stored_value.store(GetByte(encoded, startIndex))
+
+    def encode(self) -> Expr:
+        return SetByte(Bytes(b"\x00"), Int(0), self.stored_value.load())
+
+
+class AccountTypeSpec(ReferenceTypeSpec):
     def new_instance(self) -> "Account":
         return Account()
 
@@ -29,7 +122,7 @@ class AccountTypeSpec(UintTypeSpec):
 AccountTypeSpec.__module__ = "pyteal"
 
 
-class Account(Uint):
+class Account(ReferenceType):
     def __init__(self) -> None:
         super().__init__(AccountTypeSpec())
 
@@ -40,10 +133,7 @@ class Account(Uint):
 Account.__module__ = "pyteal"
 
 
-class AssetTypeSpec(UintTypeSpec):
-    def __init__(self):
-        super().__init__(8)
-
+class AssetTypeSpec(ReferenceTypeSpec):
     def new_instance(self) -> "Asset":
         return Asset()
 
@@ -60,7 +150,7 @@ class AssetTypeSpec(UintTypeSpec):
 AssetTypeSpec.__module__ = "pyteal"
 
 
-class Asset(Uint):
+class Asset(ReferenceType):
     def __init__(self) -> None:
         super().__init__(AssetTypeSpec())
 
@@ -71,10 +161,7 @@ class Asset(Uint):
 Asset.__module__ = "pyteal"
 
 
-class ApplicationTypeSpec(UintTypeSpec):
-    def __init__(self):
-        super().__init__(8)
-
+class ApplicationTypeSpec(ReferenceTypeSpec):
     def new_instance(self) -> "Application":
         return Application()
 
@@ -91,7 +178,7 @@ class ApplicationTypeSpec(UintTypeSpec):
 ApplicationTypeSpec.__module__ = "pyteal"
 
 
-class Application(Uint):
+class Application(ReferenceType):
     def __init__(self) -> None:
         super().__init__(ApplicationTypeSpec())
 
