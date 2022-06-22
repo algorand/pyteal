3818ae35932b4544b9fc560418bc554736b22d5b
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-06-22 11:20:28 -0400

    fmt

--- pyteal/ast/abi/type.py
+++ pyteal/ast/abi/type.py
@@ -142,9 +142,8 @@ class BaseType(ABC):
             )
         return value.store_into(self)
 
-    def __str__(self)->str:
+    def __str__(self) -> str:
         return str(self.__class__.__name__)
-
 
 
 BaseType.__module__ = "pyteal"
