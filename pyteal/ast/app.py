52396c2e4abf57be7d4502ff9853b110cf7d5726
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-07-01 12:16:37 -0400

    fix misspelling of uint (#431)
    * fix misspelling of uint

--- pyteal/ast/app.py
+++ pyteal/ast/app.py
@@ -249,7 +249,7 @@ class AppParam:
         )
 
     @classmethod
-    def globalNumUnit(cls, app: Expr) -> MaybeValue:
+    def globalNumUint(cls, app: Expr) -> MaybeValue:
         """Get the number of uint64 values allowed in Global State for the application.
 
         Args:
@@ -260,7 +260,7 @@ class AppParam:
         return MaybeValue(
             Op.app_params_get,
             TealType.uint64,
-            immediate_args=["AppGlobalNumUnit"],
+            immediate_args=["AppGlobalNumUint"],
             args=[app],
         )
 
@@ -281,7 +281,7 @@ class AppParam:
         )
 
     @classmethod
-    def localNumUnit(cls, app: Expr) -> MaybeValue:
+    def localNumUint(cls, app: Expr) -> MaybeValue:
         """Get the number of uint64 values allowed in Local State for the application.
 
         Args:
@@ -292,7 +292,7 @@ class AppParam:
         return MaybeValue(
             Op.app_params_get,
             TealType.uint64,
-            immediate_args=["AppLocalNumUnit"],
+            immediate_args=["AppLocalNumUint"],
             args=[app],
         )
 
