aa0e1c374c5a5ba22145afb2c974d8a9b8579934
Jacob Daitzman <jdtzmn@gmail.com>
2022-07-20 16:22:03 -0400

    Raise exception instead of returning `None`

--- pyteal/ast/seq.py
+++ pyteal/ast/seq.py
@@ -98,12 +98,14 @@ def use_seq_if_multiple(*exprs: Expr) -> Expr:
 def use_seq_if_multiple(*exprs):
     """If multiple expressions are provided, wrap them in a Seq expression."""
 
+    # Guard against no expressions
+    if len(exprs) == 0:
+        raise TealInputError("Expressions cannot be empty.")
+
     # Handle case where a list of expressions is provided
     if len(exprs) == 1 and isinstance(exprs[0], list):
         exprs = exprs[0]
 
     if len(exprs) > 1:
         return Seq(*exprs)
-    if len(exprs) == 0:
-        return None
     return exprs[0]
