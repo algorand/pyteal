ac23c4aa53872f5805cedde6b096dc13f549e1e0
michaeldiamant <michaeldiamant@users.noreply.github.com>
2022-07-21 20:52:44 -0400

    Update CHANGELOG.md

---
+++ examples/application/sandbox.py
@@ -0,0 +1,15 @@
+# import pyteal.ast.jsonref
+# from pyteal import *
+#
+# if __name__ == "__main__":
+#
+#     print(JsonRef(pyteal.ast.jsonref.JsonRefType.uint64, Bytes("foo"), Bytes("key")))
+#
+#     # print(
+#     # compileTeal(
+#     #         Seq(
+#     #             # BoxCreate(Bytes("foo"), Int(2)),
+#     #             BoxReplace(Bytes("foo"), Int(2), Bytes("Replacement")),
+#     #             Int(1)
+#     #         ), mode=Mode.Application, version=7
+#     #     ))
