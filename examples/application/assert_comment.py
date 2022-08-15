b2adcebb94bb59a2a377d3feed6286dde8da73a8
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-08-15 08:16:12 -0400

    Adding example program

---
+++ examples/application/assert_comment.py
@@ -0,0 +1,19 @@
+from pyteal import *
+
+
+@Subroutine(TealType.uint64)
+def tst_subr():
+    return Seq(Assert((Int(0), "newp")), Int(1))
+
+
+program = Seq(
+    # Make this be
+    Assert((Int(1), "ok")),
+    # On different
+    Assert((Int(1), "no way"), (Int(1), "wow")),
+    # lines
+    tst_subr(),
+)
+
+
+print(compileTeal(program, mode=Mode.Application, version=7, debug=True))
