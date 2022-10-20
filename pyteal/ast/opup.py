14e17a21429366cd0dd5091a3539c4a7de9e6aa2
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-10-20 07:55:49 -0400

    remove unused var

--- pyteal/ast/opup.py
+++ pyteal/ast/opup.py
@@ -15,7 +15,6 @@ from pyteal.types import TealType, require_type
 from enum import Enum
 
 ON_CALL_APP = Bytes("base16", "068101")  # v6 program "int 1"
-MIN_TXN_FEE = Int(1000)
 
 
 class OpUpMode(Enum):
