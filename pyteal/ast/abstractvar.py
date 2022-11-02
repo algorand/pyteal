25a98a494523cbd2efbe47f3ddf873b50724b845
Hang Su <hang.su@algorand.com>
2022-11-02 12:53:59 -0400

    abstractvar.py

---
+++ pyteal/ast/abstractvar.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from pyteal.types import TealType
+from pyteal.ast.expr import Expr
+
+
+class AbstractVar(ABC):
+    @abstractmethod
+    def store(self, value: Expr) -> Expr:
+        pass
+
+    @abstractmethod
+    def load(self) -> Expr:
+        pass
+
+    @abstractmethod
+    def storage_type(self) -> TealType:
+        pass
+
+
+AbstractVar.__module__ = "pyteal"
