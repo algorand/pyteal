eb7be56c216ee09e9640995c6a8360af1d3a3785
Zeph Grunschlag <zeph@algorand.com>
2022-11-07 18:06:42 -0600

    more api fixes

--- tests/integration/graviton_test.py
+++ tests/integration/graviton_test.py
@@ -537,7 +537,7 @@ def blackbox_test_runner(
 
         invariant = Invariant(predicate, name=f"{case_name}[{i}]@{mode}-{dr_prop}")
         print(f"{i+1}. Assertion for {case_name}-{mode}: {dr_prop} <<{predicate}>>")
-        invariant.validates(dr_prop, inputs, inspectors)
+        invariant.validates(dr_prop, inspectors)
 
 
 # ---- Graviton / Blackbox tests ---- #
@@ -729,7 +729,7 @@ def blackbox_pyteal_example3():
 
     # Assert that each invariant holds on the sequences of inputs and dry-runs:
     for property, predicate in predicates.items():
-        Invariant(predicate).validates(property, inputs, inspectors)
+        Invariant(predicate).validates(property, inspectors)
 
 
 def blackbox_pyteal_example4():
