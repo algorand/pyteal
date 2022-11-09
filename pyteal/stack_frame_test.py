202b0000790b10d11cac97d29a7af54022a47798
Zeph Grunschlag <zeph@algorand.com>
2022-11-08 20:15:47 -0600

    first stack_frame_test

--- pyteal/stack_frame_test.py
+++ pyteal/stack_frame_test.py
@@ -1,3 +1,25 @@
+from inspect import FrameInfo
+from unittest.mock import Mock
+
+from pyteal.stack_frame import StackFrame
+
 """
 TODO: some unit tests
 """
+
+
+def test_is_pyteal():
+    FrameInfo = Mock()
+    FrameInfo.return_value = Mock()
+
+    FrameInfo.return_value.filename = "not_pyteal.py"
+    sf = StackFrame(FrameInfo(), None)
+    assert not sf._is_pyteal()
+
+    FrameInfo.return_value.filename = "blahblah/pyteal/ir/blah"
+    sf = StackFrame(FrameInfo(), None)
+    assert sf._is_pyteal()
+
+    FrameInfo.return_value.filename = "blahblah/pyteal/not_really..."
+    sf = StackFrame(FrameInfo(), None)
+    assert not sf._is_pyteal()
