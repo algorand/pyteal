b727f96cb73bf66f4883b2ea5347cb9df9eab65e
Zeph Grunschlag <zeph@algorand.com>
2023-02-01 23:14:37 -0600

    Small tweaks of Router.bare_calls

--- pyteal/ast/router.py
+++ pyteal/ast/router.py
@@ -35,6 +35,8 @@ from pyteal.ast.naryexpr import And, Or
 from pyteal.ast.txn import Txn
 from pyteal.ast.return_ import Approve, Reject
 
+ActionType = Expr | SubroutineFnWrapper | ABIReturnSubroutine
+
 
 class CallConfig(IntFlag):
     """
@@ -130,9 +132,6 @@ class MethodConfig:
 
 
 MethodConfig.__module__ = "pyteal"
-
-
-ActionType = Expr | SubroutineFnWrapper | ABIReturnSubroutine
 
 
 @dataclass(frozen=True)
@@ -805,7 +804,7 @@ class Router:
         self.method_sig_to_selector: dict[str, bytes] = dict()
         self.method_selector_to_sig: dict[bytes, str] = dict()
 
-        self.bare_calls: BareCallActions | None = bare_calls
+        self.bare_call_actions: BareCallActions = bare_calls or BareCallActions()
 
     def _clean(self) -> None:
         self.approval_ast._clean_bare_calls()
@@ -983,8 +982,8 @@ class Router:
             * clear_state_program: an AST for clear-state program
             * contract: a Python SDK Contract object to allow clients to make off-chain calls
         """
-        if self.bare_calls and not self.bare_calls.is_empty():
-            bare_call_approval = self.bare_calls.approval_construction()
+        if not self.bare_call_actions.is_empty():
+            bare_call_approval = self.bare_call_actions.approval_construction()
             if bare_call_approval:
                 self.approval_ast.bare_calls = [
                     CondNode(
