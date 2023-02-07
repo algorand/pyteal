5dd9b8e0caf5663e2b124ad7ba3a282594c568d4
Zeph Grunschlag <tzaffi@users.noreply.github.com>
2023-02-07 09:53:20 -0600

    Update pyteal/compiler/compiler_test.py
    Co-authored-by: Hang Su <87964331+ahangsu@users.noreply.github.com>

--- pyteal/compiler/compiler_test.py
+++ pyteal/compiler/compiler_test.py
@@ -2348,7 +2348,7 @@ def router_app_tester() -> tuple[list[pt.Router], dict[str, str]]:
     assert "Frame pointers aren't available" in str(e.value)
 
     def router_compilarison(router, version, fixture_ap, fixture_csp, key, write=False):
-        (actual_ap, actual_csp, _) = router.compile_program(version=version)
+        actual_ap, actual_csp, _ = router.compile_program(version=version)
 
         # if write:
         #     with open(ROUTER_FIXTURES / fixture_ap, "w") as f:
