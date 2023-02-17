4fa06fabb987fa3e4d2ce6bd22f86e81fdd373bc
Hang Su <87964331+ahangsu@users.noreply.github.com>
2023-02-17 14:49:01 -0500

    update comments
    Co-authored-by: Zeph Grunschlag <tzaffi@users.noreply.github.com>

--- pyteal/ast/router.py
+++ pyteal/ast/router.py
@@ -654,7 +654,7 @@ class ASTBuilder:
         - handler argument instance generate
         - handler argument decode from Txn.application_args (with use_frame_pt=True option on)
         - generating execution branch that calls the handler and handles handler return.
-          NOTE: the very last step is handled different from pre-frame-pt version, see illustration below:
+          NOTE: the very last step is handled differently from the frame pointer version; see the illustration below:
 
         |
         | main execution
