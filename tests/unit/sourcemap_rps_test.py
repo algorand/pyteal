7f8addfe2de34b6f214107b4c90ec8e97bcd3ac6
Zeph Grunschlag <zeph@algorand.com>
2023-02-22 22:25:36 -0600

    test aimed at resolving Source Mapper: Poor Mapping of Subroutines #658

--- tests/unit/sourcemap_rps_test.py
+++ tests/unit/sourcemap_rps_test.py
@@ -1,8 +1,18 @@
+from configparser import ConfigParser
 from pathlib import Path
+from unittest import mock
 
 import pytest
 
 RPS = Path.cwd() / "tests" / "teal"
+
+
+@pytest.fixture
+def mock_ConfigParser():
+    patcher = mock.patch.object(ConfigParser, "getboolean", return_value=True)
+    patcher.start()
+    yield
+    patcher.stop()
 
 
 def compare_and_assert(file, actual):
@@ -19,6 +29,33 @@ def no_regressions_rps():
 
     compiled = compileTeal(approval_program(), Mode.Application, version=6)
     compare_and_assert(RPS / "rps.teal", compiled)
+
+
+@pytest.mark.serial
+def test_annotated_rps(mock_ConfigParser):
+    from pyteal import Compilation, Mode
+    from pyteal.compiler.sourcemap import _PyTealSourceMapper
+    from tests.teal.rps import approval_program
+
+    with open(RPS / "rps.teal", "r") as f:
+        expected_teal = f.read()
+
+    with open(RPS / "rps_annotated.teal", "r") as f:
+        expected_annotated = f.read()
+
+    cbundle = Compilation(approval_program(), Mode.Application, version=6).compile(
+        with_sourcemap=True,
+        annotate_teal=True,
+        annotate_teal_headers=True,
+        annotate_teal_concise=False,
+    )
+    teal, annotated = cbundle.teal, cbundle.sourcemap.annotated_teal
+
+    assert expected_teal == teal
+    assert expected_annotated == annotated
+    _PyTealSourceMapper._validate_annotated(
+        False, teal.splitlines(), annotated.splitlines()
+    )
 
 
 @pytest.mark.serial
