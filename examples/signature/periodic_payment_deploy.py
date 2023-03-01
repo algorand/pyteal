df7c968fbfea536aec3422b95e7ba29f8cc19386
udaykiran <40133834+uday7981@users.noreply.github.com>
2023-03-01 16:47:33 +0530

    Update issue30

--- examples/signature/periodic_payment_deploy.py
+++ examples/signature/periodic_payment_deploy.py
@@ -1,41 +1,54 @@
+import base64
+import uuid
 
-
-
-from algosdk import algod, account, mnemonic, transaction
-from algosdk.future import template
+from algosdk import account, algod, mnemonic, transaction
+from algosdk.future import transaction as future
 from pyteal import *
 
-# Compile and sign the smart contract
-teal_source = compileTeal(periodic_payment(), mode=Mode.Signature, version=2)
-compiled = compileProgram(teal_source)
-
-# Create an Algod client
-acl = algod.AlgodClient(
-    algod_token="YOUR_ALGOD_TOKEN", algod_address="YOUR_ALGOD_ADDRESS"
-)
+# set account information and get algod client
+algod_token = "your_algod_token"
+algod_address = "http://localhost:4001"
+acl = algod.AlgodClient(algod_token, algod_address)
 
 # Recover the account that is wanting to delegate signature
 passphrase = "patrol crawl rule faculty enemy sick reveal embody trumpet win shy zero ill draw swim excuse tongue under exact baby moral kite spring absent double"
 sk = mnemonic.to_private_key(passphrase)
 addr = account.address_from_private_key(sk)
+print(f"Dispense at least 201000 microAlgo to {addr}")
+input("Make sure you did that. Press Enter to continue...")
 
-# Get suggested parameters for the transaction
+# compile the Teal program
+teal_source = compileTeal(periodic_payment(), mode=Mode.Signature, version=2)
+response = acl.compile(teal_source.encode("utf-8"))
+program = response["result"]
+
+# create the logic signature
+lsig = transaction.LogicSig(program)
+
+# sign the logic signature with an account sk
+lsig.sign(sk)
+
+# get suggested parameters
 params = acl.suggested_params()
-params.flat_fee = True
-params.fee = 1000
-
-# Create the contract account
-contract = template.LogicSig(compiled)
-
-# Create a payment transaction
-receiver = "ZZAF5ARA4MEC5PVDOP64JM5O5MQST63Q2KOY2FLYFLXXD3PFSNJJBYAFZM"
+gen = params["genesis_id"]
+gh = params["genesis_hash"]
+start_round = params["last_round"] - (params["last_round"] % 1000)
+end_round = start_round + 1000
+fee = 1000
 amount = 200000
+receiver = "ZZAF5ARA4MEC5PVDOP64JM5O5MQST63Q2KOY2FLYFLXXD3PFSNJJBYAFZM"
 lease = base64.b64decode("y9OJ5MRLCHQj8GqbikAUKMBI7hom+SOj8dlopNdNHXI=")
-txn = transaction.PaymentTxn(addr, params, receiver, amount, lease)
 
-# Sign the transaction with the contract account
-stxn = txn.sign(contract)
+# create the transaction
+txn = future.PaymentTxn(addr, fee, start_round, end_round, gh, receiver, amount, flat_fee=True, lease=lease)
 
-# Send the transaction to the network
-txid = acl.send_transaction(stxn)
-print("Transaction ID:", txid)
+# create the logic signature transaction
+lstx = future.LogicSigTransaction(txn, lsig)
+
+# write to file
+txns = [lstx]
+transaction.write_to_file(txns, "p_pay.stxn")
+
+# send the transaction to the network
+txid = acl.send_transaction(lstx)
+print(f"Transaction ID: {txid}")
--- examples/signature/recurring_swap_deploy.py
+++ examples/signature/recurring_swap_deploy.py
@@ -1,68 +1,57 @@
+#!/usr/bin/env python3
+
 import base64
-from nacl import encoding, hash
 import params
+import uuid
 
-import time
-
-
-from algosdk import algod, account, encoding as algosdk_encoding, transaction
+from algosdk import account, algod, mnemonic, transaction
 from pyteal import *
 
 from recurring_swap import recurring_swap
 
-# ------- generate provider's account -----------------------------------------------
-private_key, provider_addr = account.generate_account()
-print("provider addr: {}".format(provider_addr))
+# Compile TEAL source code
+teal_source = compileTeal(recurring_swap(), mode=Mode.Signature, version=2)
 
-# ------- instantiate template, compile teal source, get escrow address -------------
+# Compile TEAL program with Python SDK
+compiled_teal = transaction.compile_teal(teal_source)
 
-program = recurring_swap(tmpl_provider=Addr(provider_addr))
-teal_source = compileTeal(program, mode=Mode.Signature, version=2)
-# print(teal_source)
-
-# compile teal
-teal_bytes = teal_source.encode()
-escrow_lsig = transaction.LogicSig(account.logic_sign(teal_bytes))
-
-escrow_addr = algosdk_encoding.encode_address(escrow_lsig.address())
-print("Dispense at least 202000 microAlgo to {}".format(escrow_addr))
-input("Make sure you did that. Press Enter to continue...")
-
-# now, as a provider, you can withdraw Algo from the escrow if you sign the first valid
+# Create an algod client
 acl = algod.AlgodClient(params.algod_token, params.algod_address)
 
-sp = acl.suggested_params_as_object()
-first_valid = sp.first
-data = first_valid.to_bytes(8, byteorder="big")
-lease = hash.sha256(data)
-lease_bytes = encoding.HexEncoder.decode(lease)
-print("first valid: {}".format(first_valid))
+# Recover the account that is wanting to delegate signature
+passphrase = "patrol crawl rule faculty enemy sick reveal embody trumpet win shy zero ill draw swim excuse tongue under exact baby moral kite spring absent double"
+sk = mnemonic.to_private_key(passphrase)
+addr = account.address_from_private_key(sk)
+print("Dispense at least 201000 microAlgo to {}".format(addr))
+input("Make sure you did that. Press Enter to continue...")
 
-txn = transaction.PaymentTxn(escrow_addr, sp, provider_addr, 100000, lease=lease_bytes)
+# Get suggested parameters
+params = acl.suggested_params()
+gen = params["genesisID"]
+gh = params["genesishashb64"]
+startRound = params["lastRound"] - (params["lastRound"] % 1000)
+endRound = startRound + 1000
+fee = 1000
+amount = 200000
+receiver = "ZZAF5ARA4MEC5PVDOP64JM5O5MQST63Q2KOY2FLYFLXXD3PFSNJJBYAFZM"
+lease = base64.b64decode("y9OJ5MRLCHQj8GqbikAUKMBI7hom+SOj8dlopNdNHXI=")
 
-lstx = transaction.LogicSigTransaction(txn, escrow_lsig)
+# Create a transaction
+txn = transaction.PaymentTxn(addr, fee, startRound, endRound, gh, receiver, amount, flat_fee=True, lease=lease)
 
-assert lstx.verify()
+# Create a logic signature from compiled TEAL program
+lsig = transaction.LogicSig(compiled_teal)
 
+# Sign the logic signature with an account sk
+lsig.sign(sk)
+
+# Create a logic signature transaction with the contract account LogicSig
+lstx = transaction.LogicSigTransaction(txn, lsig)
+
+# Write the transaction to a file
+txns = [lstx]
+transaction.write_to_file(txns, "r_swap.stxn")
+
+# Send the raw LogicSigTransaction to the network
 txid = acl.send_transaction(lstx)
-print("1st withdraw Succesfull! txid:{}".format(txid))
-
-# at least sleep to the next round
-time.sleep(6)
-
-sp = acl.suggested_params_as_object()
-first_valid = sp.first
-data = first_valid.to_bytes(8, byteorder="big")
-lease = hash.sha256(data)
-lease_bytes = encoding.HexEncoder.decode(lease)
-
-print("first valid: {}".format(first_valid))
-
-txn = transaction.PaymentTxn(escrow_addr, sp, provider_addr, 100000, lease=lease_bytes)
-
-lstx = transaction.LogicSigTransaction(txn, escrow_lsig)
-
-assert lstx.verify()
-
-txid = acl.send_transaction(lstx)
-print("2nd withdraw Succesfull! txid:{}".format(txid))
+print("Transaction ID: " + txid)
