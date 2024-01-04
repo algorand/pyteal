cfa51ae416c3817bace2975ba360a55abf56ab27
Jason Paulos <jasonpaulos@users.noreply.github.com>
2024-01-04 10:10:08 -0500

    Fix uint64 return types on ec ops

--- pyteal/ast/ec_test.py
+++ pyteal/ast/ec_test.py
@@ -10,23 +10,24 @@ OPERATIONS: list[
         | Callable[[pt.EllipticCurve, pt.Expr, pt.Expr], pt.Expr],
         pt.Op,
         int,
+        pt.TealType,
     ]
 ] = [
-    (pt.EcAdd, pt.Op.ec_add, 2),
-    (pt.EcScalarMul, pt.Op.ec_scalar_mul, 2),
-    (pt.EcPairingCheck, pt.Op.ec_pairing_check, 2),
-    (pt.EcMultiScalarMul, pt.Op.ec_multi_scalar_mul, 2),
-    (pt.EcSubgroupCheck, pt.Op.ec_subgroup_check, 1),
-    (pt.EcMapTo, pt.Op.ec_map_to, 1),
+    (pt.EcAdd, pt.Op.ec_add, 2, pt.TealType.bytes),
+    (pt.EcScalarMul, pt.Op.ec_scalar_mul, 2, pt.TealType.bytes),
+    (pt.EcPairingCheck, pt.Op.ec_pairing_check, 2, pt.TealType.uint64),
+    (pt.EcMultiScalarMul, pt.Op.ec_multi_scalar_mul, 2, pt.TealType.bytes),
+    (pt.EcSubgroupCheck, pt.Op.ec_subgroup_check, 1, pt.TealType.uint64),
+    (pt.EcMapTo, pt.Op.ec_map_to, 1, pt.TealType.bytes),
 ]
 
 
 def test_EcOperation():
-    for operation, expected_op, num_args in OPERATIONS:
+    for operation, expected_op, num_args, expected_return_type in OPERATIONS:
         for curve in pt.EllipticCurve:
             args = [pt.Bytes(f"arg{i}") for i in range(num_args)]
             expr = operation(curve, *args)
-            assert expr.type_of() == pt.TealType.bytes
+            assert expr.type_of() == expected_return_type
 
             expected = pt.TealSimpleBlock(
                 [pt.TealOp(arg, pt.Op.byte, f'"arg{i}"') for i, arg in enumerate(args)]
