af949aef36f6a7c67b8617e77e2e6c53561b0c8b
Gary Malouf <982483+gmalouf@users.noreply.github.com>
2025-02-25 15:44:05 -0500

    Prep for release v0.27.0. (#726)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyteal",
-    version="0.26.1",
+    version="0.27.0",
     author="Algorand",
     author_email="pypiservice@algorand.com",
     description="Algorand Smart Contracts in Python",
